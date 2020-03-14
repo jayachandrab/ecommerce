0c44c666ac527aba9a360ddcd8017ea635b5fae3
Jayachandra <jayachandra1805@gmail.com>
2020-03-14 12:33:33 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductlistComponent } from './productlist/productlist.component';
+import { AppnavComponent } from './appnav/appnav.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import { PageNotfoundComponent } from './page-notfound/page-notfound.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TruncatePipe } from './services/truncat.pipe';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductlistComponent,
+    AppnavComponent,
+    CheckoutComponent,
+    CartComponent,
+    PageNotfoundComponent,
+    TruncatePipe,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
